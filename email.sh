0b67cca14ec385eef88b985f35e13be864067ad9
vivek kumar <vivekhaarit@gmail.com>
2020-04-01 00:09:19 +0530

    [vivek]Added:ensured dot after @xxx & subdomain

--- email.sh
+++ email.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "Type the email"
+
 read email
-pattern="^[a-z]{1,}[@][a-z0-9]{1,}"
+pattern="^[a-z]{1,}[0-9]*[@][a-z0-9]+[.][a-z]{1,}"
 if [[ $email =~ $pattern ]]
 then 
 	echo "valid"
